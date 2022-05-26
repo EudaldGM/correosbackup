601b722f407823c26dd2689e14fdf19dd6926cdc
EuGM <39499552+EudaldGM@users.noreply.github.com>
2022-05-26 16:26:20 +0200

    Update backup_tags_script.sh

--- backup_tags_script.sh
+++ backup_tags_script.sh
@@ -6,12 +6,12 @@ buplan_daily1week=$(aws backup create-backup-plan --cli-input-json file://awir-p
 buplan_dailymonthly1year=$(aws backup create-backup-plan --cli-input-json file://awir-p-devops-bkp-dailymonthly1year.json --output text | awk '{print $2}')
 buplan_dailyweekly1month=$(aws backup create-backup-plan --cli-input-json file://awir-p-devops-bkp-dailyweekly1month.json --output text | awk '{print $2}')
 
-give a-ok to the impersonation of the role by backup.amazonaws.com
+###give a-ok to the impersonation of the role by backup.amazonaws.com
 aws iam update-assume-role-policy --role-name awir-p-devops-iar-backup --policy-document file://policy_assumption.json
 
 accountID=$(aws sts get-caller-identity --query "Account" --output text)
 
-#for the resource assignation set the correct account ID for each account
+###for the resource assignation set the correct account ID for each account
 sed -i "s/iam::.*:role/iam::$accountID:role/g" *.json
 
 ###Assign resources
